7d9dce3d6f932a5990334f657ea67fd64e707644
Niklas Hedlund <noja1989@gmail.com>
2020-08-02 13:47:45 +0200

    Added dummy data

--- client/src/app.vue
+++ client/src/app.vue
@@ -1,24 +1,100 @@
 <template>
   <div class="container">
-    <h1>{{ message }}</h1>
+    <h1>Tasks for today</h1>
+
+    <div class="task-list-container">
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+      <div class="task-container">
+          <span>Clean the stuff</span>
+          <button class="complete-task-button" type="button">
+              Complete
+          </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      message: 'Using Parcel In A Vue.js App',
+      message: "Using Parcel In A Vue.js App",
     };
   },
 };
 </script>
 
-<style scoped>
-  .container {
-    width: 600px;
-    margin: 50px auto;
-    text-align: center;
-  }
+<style lang="scss" scoped>
+.container {
+  width: 600px;
+  margin: 50px auto;
+  text-align: center;
+}
+
+.task-list-container{
+    .task-container {
+        width:100%;
+        text-align: left;
+        margin-bottom: 10px;
+
+        .complete-task-button {
+            background: none;
+            float: right;
+        }
+
+        &:after {
+            display:block;
+            content:'';
+            clear:both;
+        }
+    }
+}
 </style>
